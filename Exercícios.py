2d207c4326c536fedac646a6faff4b6a8f428d80
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-07-03 18:29:59 -0300

    Add files via upload

---
+++ Exercícios.py
@@ -0,0 +1,44 @@
+# Mini exercícios
+
+#ler um número inteiro e imprimir falando o tipo do mesmo
+
+num = 50
+print(num)
+print(type(num))
+
+# Ler um numero real e imprimir mostrando o tipo do mesmo
+
+num1 = 12.50
+print(num1)
+print(type(num1))
+
+# Coletar 3 números do usuário - somar e imprimir a soma
+
+num2 = int(input("digite um numero inteiro"))
+num3 = int(input("digite um numero inteiro"))
+num4 = int(input("digite um numero inteiro"))
+
+soma = num2 + num3 + num4
+print(soma)
+
+# raiz quadrada de número real
+num5 = 12.50**2
+print(num5)
+
+# quinta parte de um número real
+num6 = 500.25%5
+print(num6)
+
+# Ler uma temperatura em graus celsius e apresentar ela convertida em graus fahrenheit
+
+temp = 20 * 9/5
+print(temp)
+temp = 36 + 32
+print(temp)
+print(f'20 graus convertido em Fahrenheit é igual a {temp}!')
+
+# Ler a temperatura em Fahrenheit e converter para graus celsius
+# F = 100
+temp2 = 5 * (100 - 32) / 9
+print(temp2)
+print(f' 100 Fahrenheit convertido em celsius é igual a {temp}!')
